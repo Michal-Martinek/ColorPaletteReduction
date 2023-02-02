bae3a287cf41350e56ea18641c341578462b1236
Michal Martínek <michal7952@seznam.cz>
2023-02-02 16:24:30 +0100

    Implemented debug cost graph

--- ColorPaletteReduction.py
+++ ColorPaletteReduction.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 
 os.chdir(os.path.dirname(__file__))
+DEBUG = False
 
 def getDistances(colors, means):
 	diff = colors[:, np.newaxis].astype('int32') - means[np.newaxis].astype('int32')
@@ -17,27 +18,40 @@ def adjustClusterCenters(colors, means, k) -> int:
 			continue
 		means[meanIdx] = np.sum(clusterPoints, axis=0) / clusterPoints.shape[0]
 	return sum([np.where(clusterIdx == meanIdx, dists[:, meanIdx], 0).sum(dtype='int64') for meanIdx in range(k)])
-def initMeans(colors, k):
+def initMeans(colors, k): # TODO: better means init
 	withoutRepeats = np.unique(colors, axis=0)
 	assert withoutRepeats.shape[0] > k, 'the k is too big'
 	meanIdxs = np.random.choice(np.arange(withoutRepeats.shape[0]), k, replace=False)
 	return withoutRepeats[meanIdxs]
-def doKMeans(colors, k, maxIterations):
+def doKMeans(colors, k, maxIterations, autoStop=not DEBUG):
 	means = initMeans(colors, k)
+	lastCosts = []
+
 	lastCost = -1
 	bestMeans = None
 	bestCost = float('inf')
-	for i in range(maxIterations):
+	for i in range(maxIterations): # TODO: try to guess the optimal stopping point based on the costs
 		cost = adjustClusterCenters(colors, means, k)
-		print(cost)
-
+		lastCosts.append(cost)
 		if cost < bestCost:
 			bestCost = cost
 			bestMeans = means.copy()
-		if cost == lastCost:
+		if autoStop and cost == lastCost:
 			break
 		lastCost = cost
-	return bestMeans
+	return bestMeans, lastCosts
+def applyColorPalette(img, colors, means):
+	dists = getDistances(colors, means)
+	clusterIdx = np.argmin(dists, axis=1)
+	return means[clusterIdx].reshape((img.shape[0], img.shape[1], 3))
+def getCostsDiagram(costs):
+	desiredHeight = 300
+	scale = desiredHeight / max(costs)
+	img = np.zeros((desiredHeight, len(costs), 3), dtype='uint8')
+	heights = desiredHeight - (np.array(costs) * scale).astype('int32')
+	heights = np.minimum(heights, desiredHeight-1)
+	img[heights, np.arange(len(costs))] = [255, 255, 255]
+	return img
 
 def mouse_click(event, x, y, flags, param):
 	if event == cv2.EVENT_LBUTTONDOWN:
@@ -45,14 +59,14 @@ def mouse_click(event, x, y, flags, param):
 def main():
 	img = cv2.imread('bird-original.png')
 	colors = img.reshape((-1, 3))
-	means = doKMeans(colors, 3, 10)
-	
-	dists = getDistances(colors, means)
-	clusterIdx = np.argmin(dists, axis=1)
-	reducedPalette = means[clusterIdx].reshape((img.shape[0], img.shape[1], 3))
+	means, costs = doKMeans(colors, 8, 50)
+	reducedPalette = applyColorPalette(img, colors, means)
 	
 	cv2.imshow('Bird', img)
 	cv2.imshow('Reduced', reducedPalette)
+	if DEBUG:
+		diagram = getCostsDiagram(costs)
+		cv2.imshow('CostGraph', diagram)
 	cv2.setMouseCallback('Bird', mouse_click)
 	while cv2.getWindowProperty('Bird', cv2.WND_PROP_VISIBLE) >= 1:
 		cv2.waitKey(10)
